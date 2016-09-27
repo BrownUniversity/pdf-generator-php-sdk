a967d3f14c919b59d1079d73202b0183b6598912
Christopher Keith <Christopher_Keith@brown.edu>
2016-09-27 13:06:50 -0400

    Adding credentials and rough implementation as a POC

--- src/Pdfgen.php
+++ src/Pdfgen.php
@@ -26,6 +26,41 @@ class Pdfgen {
     public function __construct($logger)
     {
         $this->logger = $logger;
+        $this->username = '***REMOVED***';
+        $this->password = '***REMOVED***';
+        $this->base_url = 'https://local.cis-dev.brown.edu:8443/pdf/';
     }
-    
+
+    public function convert($html, $css = array(), $js = array())
+    {
+        $url = $this->base_url . 'convert';
+
+        $ch = curl_init();
+
+        $options = [];
+        $options[CURLOPT_URL] = $url;
+        $options[CURLOPT_USERPWD] = "{$this->username}:{$this->password}";
+        $options[CURLOPT_SSL_VERIFYPEER] = false;
+        $options[CURLOPT_RETURNTRANSFER] = true;
+        $options[CURLINFO_HEADER_OUT] = true;
+        $options[CURLOPT_POST] = true;
+        $options[CURLOPT_POSTFIELDS] = array(
+            'html' => $html,
+            'css' => implode("|", $css),
+            'js' => implode("|", $js)
+        );
+
+        curl_setopt_array($ch, $options);
+
+        $result = curl_exec($ch);
+        $info = curl_getinfo($ch);
+        $error = curl_errno($ch);
+        $errmsg = curl_error($ch);
+        $this->logger->debug('CURL INFO', $info);
+        $this->logger->debug($errmsg);
+        $this->logger->debug('Result', ['contents' => $result]);
+        $fp = fopen('/tmp/output.pdf', 'w+');
+        fwrite($fp, $result);
+        return '/tmp/output.pdf';
+    }
 }
